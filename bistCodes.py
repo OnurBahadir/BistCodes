58c28d2ac09c9197014d690fdcf659cc0fb7f92d
Onur Bahadır <onurbahadirr@gmail.com>
2020-07-29 05:55:18 +0300

    Add files via upload

---
+++ bistCodes.py
@@ -0,0 +1,12 @@
+
+import bs4 as bs
+import requests 
+
+def getBistCodes():
+    req=requests.get("https://www.kap.org.tr/tr/bist-sirketler")
+    soup=bs.BeautifulSoup(req.text,"html.parser")
+    codes=soup.find_all("div",{"class":"comp-cell _04 vtable"})
+    codes=[x.text[1:-1] for x in codes]
+    return codes
+
+
