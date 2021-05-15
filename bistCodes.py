81541feefae1c458c8da0519250767230ced4cd3
Onur Bahadır <onurbahadirr@gmail.com>
2021-05-15 17:43:29 +0300

    Update bistCodes.py

--- bistCodes.py
+++ bistCodes.py
@@ -6,6 +6,7 @@ def getBistCodes():
     soup=bs.BeautifulSoup(req.text,"html.parser")
     codes=soup.find_all("div",{"class":"comp-cell _02 vtable"})
     codes=[x.text[1:-1] for x in codes]
+    codes=list(dict.fromkeys(codes))  
     return codes
 
 if __name__=="__main__":
